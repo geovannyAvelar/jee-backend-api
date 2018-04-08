2d9acf0cb06efeffa33933711a9e4d93a6317a56
Geovanny Avelar <geovanny.avelar@gmail.com>
2018-04-08 08:54:46 -0300

    Project structure

---
+++ src/main/java/br/com/avelar/backend/rest/PersonApi.java
@@ -0,0 +1,21 @@
+package br.com.avelar.backend.rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.avelar.backend.rest.model.Person;
+
+@Path("/person")
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
+public interface PersonApi {
+	
+	@GET
+	public List<Person> findAll();
+	
+}
