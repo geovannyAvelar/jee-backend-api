32cfee273712204b6b312643b88075e8bb8aecf7
geovanny.carneiro <geovanny.carneiro@iacit.com.br>
2018-04-09 14:33:27 -0300

    Adding hibernate entity

--- src/main/java/br/com/avelar/backend/rest/PersonApi.java
+++ src/main/java/br/com/avelar/backend/rest/PersonApi.java
@@ -1,9 +1,12 @@
 package br.com.avelar.backend.rest;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,9 +16,15 @@ import br.com.avelar.backend.rest.model.Person;
 @Path("/person")
 @Produces({ MediaType.APPLICATION_JSON })
 @Consumes({ MediaType.APPLICATION_JSON })
-public interface PersonApi {
+public interface PersonApi extends Serializable {
+	
+	@POST @Consumes({ MediaType.APPLICATION_JSON })
+	public void savePerson(Person person);
 	
 	@GET
 	public List<Person> findAll();
+	
+	@DELETE
+	public void deletePerson(Person person);
 	
 }
